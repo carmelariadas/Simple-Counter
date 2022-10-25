bf8dbfd9648fa5a759606c7b0af6e34847f4f5d7
carmelariadas <carmen.mk.167@gmail.com>
2022-10-25 19:50:45 +0000

    Done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,33 @@
 import React from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center container mt-5">
+			<div className="row d-flex  justify-content-center">
+				
+				<div className="col-2 reloj mx-4">
+					<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-clock mt-4" viewBox="0 0 16 16">
+					<path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"/>
+					<path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z"/>
+					</svg>
+				</div>
+				<div className="digit col-1 mx-2"><p className="mt-4">{props.six}</p></div>
+				<div className="digit col-1 mx-2"><p className="mt-4">{props.five}</p></div>
+				<div className="digit col-1 mx-2"><p className="mt-4">{props.four}</p></div>
+				<div className="digit col-1 mx-2"><p className="mt-4">{props.three}</p></div>
+				<div className="digit col-1 mx-2"><p className="mt-4">{props.two}</p></div>
+				<div className="digit col-1 mx-2"><p className="mt-4">{props.one}</p></div>
+			</div>	
+			<div className="container">
+				<button type="button" className="btn btn-danger" onClick={props.iniciarParada}>Parar</button>
+				<button type="button" className="btn btn-success" onClick={props.reanudar}>Reanudar</button>
+				<button type="button" className="btn btn-primary" onClick={props.iniciarReinicio}>Reinicio</button>				
+			</div>
+
 		</div>
 	);
 };
--- src/js/index.js
+++ src/js/index.js
@@ -9,4 +9,47 @@ import "../styles/index.css";
 import Home from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+
+let counter = 0;
+let parar = false;
+let reinicio = false;
+
+
+const iniciarParada = () =>{
+	parar = true;
+}
+
+const reanudar = () =>{
+	parar = false;
+}
+
+const iniciarReinicio = () =>{
+	reinicio = true;
+}
+
+
+
+setInterval(() => {
+	const one = counter % 10;
+	const two = Math.floor (counter/10) % 10;
+	const three = Math.floor (counter/100) % 10;
+	const four = Math.floor (counter/1000) % 10;
+	const five = Math.floor (counter/10000) % 10;
+	const six = Math.floor (counter/100000) % 10;
+	
+
+	if (parar == false){
+		counter++;
+	};
+
+	if (reinicio == true){
+		counter = -1;
+		counter++;
+		reinicio = false;
+		
+	};
+
+
+    
+	ReactDOM.render(<Home one={one} two={two} three={three} four={four} five={five} six={six} iniciarParada={iniciarParada} reanudar={reanudar} iniciarReinicio={iniciarReinicio} counter={counter} />, document.querySelector("#app"));
+}, 1000);
